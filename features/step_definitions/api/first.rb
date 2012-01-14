5d1d0ca473164a4afb5c1f37caa2024b64634407
FENG Zhichao <flankerfc@gmail.com>
2012-01-15 00:28:19 +1100

    implement the database support step

--- features/step_definitions/api/first.rb
+++ features/step_definitions/api/first.rb
@@ -1,6 +1,8 @@
-Given /^database has one feed$/ do |feed|
-  # table is a Cucumber::Ast::Table
-  pending # express the regexp above with the code you wish you had
+require "hashie"
+
+Given /^database has one feed$/ do |table|
+  feed = Hashie::Mash.new(table.rows_hash)
+  Database.feed feed
 end
 
 When /^I visit the api$/ do
--- features/support/database_support.rb
+++ features/support/database_support.rb
@@ -1,5 +1,10 @@
-module database
-  def self.feed feed
+require "mongo"
 
+module Database
+  def self.feed feed
+    db = Mongo::Connection.new.db("test_db")
+    feeds = db.collection("feeds")
+    doc = feed.to_hash
+    feeds.insert doc
   end
 end
